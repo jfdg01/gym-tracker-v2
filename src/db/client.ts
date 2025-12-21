1cb16516e3fb49f5af49f920b643a7c97e13ea4e
Javier Dibo <javier.fco.dibo.gomez@gmail.com>
2025-12-21 18:47:46 +0100

    Bad commit

--- src/db/client.ts
+++
@@ -1,6 +0,0 @@
-import { openDatabaseSync } from 'expo-sqlite';
-import { drizzle } from 'drizzle-orm/expo-sqlite';
-import * as schema from './schema';
-
-export const expoDb = openDatabaseSync('gym-tracker.db');
-export const db = drizzle(expoDb, { schema });
